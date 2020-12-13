c6fba4ff29653bb7a44a0b4c543b82b28029b3a2
loki2k <jeremylagarto@gmail.com>
2020-12-13 11:37:20 -0800

    Updates

---
+++ precache-manifest.cece27939c9db1c95b5e1ae4d46bd2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02128795be04b9082455985f6a9aa5a2",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "dcbfe7a9b764506ba94a",
+    "url": "/quiz/static/css/main.50566d9f.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/quiz/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcbfe7a9b764506ba94a",
+    "url": "/quiz/static/js/main.fcfa7e46.chunk.js"
+  },
+  {
+    "revision": "dd19b6e4ede99b84197b",
+    "url": "/quiz/static/js/runtime-main.7a6c2dfa.js"
+  }
+]);
